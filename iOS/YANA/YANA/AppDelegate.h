d4929646c4b76b87eb052858d5c3f295c64edb67
Gavin Chu <gavincca@gmail.com>
2014-10-13 14:44:14 -0700

    initial iOS commit

---
+++ iOS/YANA/YANA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YANA
+//
+//  Created by Gavin Chu on 10/13/14.
+//  Copyright (c) 2014 CS169. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/YANA/YANA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YANA
+//
+//  Created by Gavin Chu on 10/13/14.
+//  Copyright (c) 2014 CS169. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
