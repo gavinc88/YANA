d4bf790680fc129924d1a696960b5d08f3b9eded
ShaneWang <wangsheng@berkeley.edu>
2014-10-21 15:53:04 -0700

    Revert 5e39755..acc27c5
    This rolls back to commit 5e39755649ee4fabeea241e79f9d4a2e9a055d22.

--- iOS/YANA/YANA/LoginViewController.m
+++ iOS/YANA/YANA/LoginViewController.m
@@ -7,24 +7,16 @@
 //
 
 #import "LoginViewController.h"
-#import "APIHelper.h"
-#import "User.h"
-#import "MainTabBarController.h"
 
 @interface LoginViewController ()
-
-@property (nonatomic,weak) User *user;
 
 @end
 
 @implementation LoginViewController
 
-APIHelper *apiHelper;
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    apiHelper = [[APIHelper alloc]init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,42 +25,11 @@ APIHelper *apiHelper;
 }
 
 - (IBAction)login:(UIButton *)sender {
-    NSDictionary *response = [apiHelper loginWithUsername:self.usernameText.text andPassword:self.passwordText.text];
-    if(response){
-        [self performSegueWithIdentifier:@"openMain" sender:self];
-    }
+    
 }
 
-- (IBAction)createUser:(UIButton *)sender {
-    NSDictionary *response = [apiHelper createUserWithUsername:self.usernameText.text andPassword:self.passwordText.text];
-    if(response){
-        [self performSegueWithIdentifier:@"openMain" sender:self];
-    }
-}
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    if ([segue.identifier isEqualToString:@"openMain"]) {
-        NSLog(@"preparingForSegue");
-        
-        MainTabBarController *vc = [segue destinationViewController];
-        vc.user = self.user;
-    } else {
-        return;
-    }
-}
-
-- (IBAction)textFieldDidEnd:(id)sender {
-    if ([sender isEqual: self.usernameText]){
-        if ([self.usernameText.text length] == 0) {
-            UIAlertView *alert = [[UIAlertView alloc]
-                                  initWithTitle:@"Invalid Username"
-                                        message:@"Username cannot be empty."
-                                        delegate:nil
-                                  cancelButtonTitle:@"OK"
-                                otherButtonTitles:nil];
-            [alert show];
-        }
-    }
+- (IBAction)createAccount:(UIButton *)sender {
+    
 }
 
 @end
--- iOS/YANA/YANA/MainTabBarController.h
+++ iOS/YANA/YANA/MainTabBarController.h
@@ -7,10 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "User.h"
 
 @interface MainTabBarController : UITabBarController
-
-@property (nonatomic,strong) User *user;
 
 @end
