175871f1f18154f9c6b19acebcf057f3de274f7a
ShaneWang <wangsheng@berkeley.edu>
2014-11-02 20:37:23 -0800

    Added RestaurantSearchViewController and OAuth

---
+++ iOS/YANA/YANA/RestaurantSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantSearchViewController.h
+//  YANA
+//
+//  Created by Shane on 11/2/14.
+//  Copyright (c) 2014 CS169. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantSearchViewController : UIViewController
+
+@end
---
+++ iOS/YANA/YANA/RestaurantSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  RestaurantSearchViewController.m
+//  YANA
+//
+//  Created by Shane on 11/2/14.
+//  Copyright (c) 2014 CS169. All rights reserved.
+//
+
+#import "RestaurantSearchViewController.h"
+
+@interface RestaurantSearchViewController ()
+
+@end
+
+@implementation RestaurantSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
